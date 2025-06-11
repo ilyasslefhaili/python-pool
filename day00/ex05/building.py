f5fa3bb955b708ec6819f8a32c49eba15b560a07
Ilyass <ilyass.lefhaili.cap@gmail.com>
2025-06-11 13:01:04 +0100

    updates

---
+++ day00/ex05/building.py
@@ -0,0 +1,39 @@
+import sys
+import string
+
+def count_text(text: str) -> tuple[int,int,int,int,int]:
+    upper     = sum([1 for c in text if c.isupper()])
+    lower     = sum([1 for c in text if c.islower()])
+    punct     = sum([1 for c in text if c in string.punctuation])
+    spaces    = text.count(" ")
+    digits    = sum([1 for c in text if c.isdigit()])
+    return upper, lower, punct, spaces, digits
+
+def main():
+    if len(sys.argv) > 2:
+        print("AssertionError: more than one argument is provided")
+        return
+    if len(sys.argv) == 2:
+        text = sys.argv[1]
+    else:
+        print("What is the text to count?")
+        try:
+            raw = sys.stdin.readline()
+            if raw.endswith("\n"):
+                text = raw.rstrip("\n") + " "
+            else:
+                text = raw
+        except EOFError:
+            text = ""
+
+    upper, lower, punct, spaces, digits = count_text(text)
+
+    print(f"The text contains {len(text)} characters:")
+    print(f"{upper} upper letters")
+    print(f"{lower} lower letters")
+    print(f"{punct} punctuation marks")
+    print(f"{spaces} spaces")
+    print(f"{digits} digits")
+
+if __name__ == "__main__":
+    main()
